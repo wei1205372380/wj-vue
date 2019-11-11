6245e6a06f5942eff5ecc56907982d59d9d4f1bd
linbowen <1205372380@qq.com>
2019-11-11 23:30:24 +0800

    写login.vue文件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,37 +1,72 @@
 <template>
-    <div>
-        <el-form :model="data" label-width="100px">
-            <el-form-item label="用户名">
-                <el-input v-model="data.username" autocomplete="off" placeholder="请输入用户名"></el-input>
+    <body id="paper">
+        <el-form class="login-container" :model="loginForm" ref="loginForm">
+            <div style="margin: 10px auto 40px">
+                <h3 style="text-align: center; color: #505458">系统登陆</h3>
+            </div>
+            <el-form-item prop="username" :rules="{required: true, message: '不能为空', trigger: 'blur'}">
+                <el-input type="text" placeholder="账户" v-model="loginForm.username" autocomplete="off"/>
             </el-form-item>
-            <el-form-item label="密码">
-                <el-input type="password" v-model="data.password" autocomplete="off" placeholder="请输入密码"></el-input>
+            <el-form-item prop="password" :rules="{required: true, message: '不能为空', trigger: 'blur'}">
+                <el-input type="password" placeholder="密码" v-model="loginForm.password" autocomplete="off"/>
             </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="submitForm('data')">提交</el-button>
-            </el-form-item>
+            <div style="display: flex; align-items: center; justify-content: center; margin-bottom: 50px">
+                <el-checkbox style="color: #505458" v-model="checked">记住密码</el-checkbox>
+            </div>
+            <el-button type="primary" style="width: 100%; background-color: #505458" @click="login()">登录</el-button>
         </el-form>
-    </div>
+    </body>
 </template>
 
 <script>
     export default {
         data() {
             return {
-                data: {
+                loginForm: {
                     username: '',
                     password: ''
-                }
+                },
+                checked: true
             }
         },
         methods: {
-            submitForm(data) {
-                console.log(this.data);
+            login() {
+                this.$refs["loginForm"].validate((valid) => {
+                    if (valid) {
+                        console.log("success")
+                    } else {
+                        console.log("fail")
+                    }
+                });
             }
         }
     }
 </script>
 
-<style scoped>
+<style>
+    #paper {
+        background: url("../assets/img/bg/eva1.jpg") no-repeat center;
+        background-size: cover;
+        position: fixed;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .login-container {
+        width: 400px;
+        height: 320px;
+        background: #fff;
+        background-clip: padding-box;
+        border: 1px solid #eaeaea;
+        border-radius: 15px;
+        box-shadow: 0 0 25px #cac6c6;
+        padding: 30px 50px;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+    }
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/login',
+      path: '/',
       name: 'login',
       component: login
     }
