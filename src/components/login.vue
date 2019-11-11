328bf33a4051592d9e4737372de944fd9d463f77
linbowen <1205372380@qq.com>
2019-11-11 18:26:33 +0800

    login.vue页面编辑

---
+++ src/components/login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <el-form :model="data" label-width="100px">
+            <el-form-item label="用户名">
+                <el-input v-model="data.username" autocomplete="off" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input type="password" v-model="data.password" autocomplete="off" placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('data')">提交</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                data: {
+                    username: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            submitForm(data) {
+                console.log(this.data);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
